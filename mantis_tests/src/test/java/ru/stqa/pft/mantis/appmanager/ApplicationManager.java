3d8fedf83e259ae63b93ea6df93b316efa1dc1bd
sanjar jarash <sjarashev@gmail.com>
2020-01-25 01:27:05 -0500

    Задание №19

--- mantis_tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
+++ mantis_tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -22,6 +22,7 @@ public class ApplicationManager {
   private FtpHelper ftp;
   private MailHelper mailHelper;
   private DbHelper dbHelper;
+  private SoapHelper soapHelper;
 
   public ApplicationManager(String browser) {
     this.browser = browser;
@@ -95,5 +96,12 @@ public class ApplicationManager {
 
   public DbHelper db() {
     return dbHelper;
+  }
+
+  public SoapHelper soap() {
+    if (soapHelper == null) {
+      soapHelper = new SoapHelper(this);
+    }
+    return soapHelper;
   }
 }
--- mantis_tests/src/test/java/ru/stqa/pft/mantis/tests/TestBase.java
+++ mantis_tests/src/test/java/ru/stqa/pft/mantis/tests/TestBase.java
@@ -1,12 +1,23 @@
 package ru.stqa.pft.mantis.tests;
 
+import biz.futureware.mantis.rpc.soap.client.IssueData;
+import biz.futureware.mantis.rpc.soap.client.MantisConnectPortType;
+import biz.futureware.mantis.rpc.soap.client.ObjectRef;
+import com.beust.jcommander.Parameter;
 import org.openqa.selenium.remote.BrowserType;
+import org.testng.SkipException;
 import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
 import ru.stqa.pft.mantis.appmanager.ApplicationManager;
 
+import javax.xml.rpc.ServiceException;
 import java.io.File;
 import java.io.IOException;
+import java.math.BigInteger;
+import java.net.MalformedURLException;
+import java.rmi.RemoteException;
 
 
 public class TestBase {
@@ -25,5 +36,25 @@ public class TestBase {
   public void tearDown() throws IOException {
     app.ftp().restore("config_inc.php.bak", "config_inc.php");
     app.stop();
+  }
+
+  public boolean isIssueOpen(int issueId) throws MalformedURLException, ServiceException, RemoteException {
+    String user = app.getProperty("web.adminLogin");
+    String password = app.getProperty("web.adminPassword");
+    MantisConnectPortType mc = app.soap().getMantisConnect();
+    IssueData issue = mc.mc_issue_get(user, password, BigInteger.valueOf(issueId));
+    int status = issue.getStatus().getId().intValue();
+    boolean open = false;
+    if (status == 10) {
+      open = true;
+    }
+    return open;
+  }
+
+  @BeforeMethod(enabled = false)
+  public void skipIfNotFixed(int issueId) throws MalformedURLException, ServiceException, RemoteException {
+    if (isIssueOpen(issueId)) {
+      throw new SkipException("Ignored because of issue " + issueId);
+    }
   }
 }
