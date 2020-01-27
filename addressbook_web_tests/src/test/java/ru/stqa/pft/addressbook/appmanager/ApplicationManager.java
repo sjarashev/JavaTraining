a8c565f632b0bb9485d9096bd59c54d1bdd4040e
sanjar jarash <sjarashev@gmail.com>
2020-01-26 19:28:11 -0500

    Реализована возможность удаленного запуска браузера

--- addressbook_web_tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook_web_tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -6,11 +6,14 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.URL;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
@@ -33,17 +36,25 @@ public class ApplicationManager {
     String target = System.getProperty("target", "local");
     properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
     dbHelper = new DbHelper();
-    switch (browser) {
-      case BrowserType.FIREFOX:
-        wd = new FirefoxDriver();
-        break;
-      case BrowserType.CHROME:
-        wd = new ChromeDriver();
-        break;
-      case BrowserType.EDGE:
-        wd = new EdgeDriver();
-        break;
+
+    if("".equals(properties.getProperty("selenium.server"))) {
+      switch (browser) {
+        case BrowserType.FIREFOX:
+          wd = new FirefoxDriver();
+          break;
+        case BrowserType.CHROME:
+          wd = new ChromeDriver();
+          break;
+        case BrowserType.EDGE:
+          wd = new EdgeDriver();
+          break;
+      }
+    } else {
+      DesiredCapabilities capabilities = new DesiredCapabilities();
+      capabilities.setBrowserName(browser);
+      wd = new RemoteWebDriver(new URL(properties.getProperty("selenium.server")), capabilities);
     }
+
     wd.manage().timeouts().implicitlyWait(70, TimeUnit.MILLISECONDS);
     wd.manage().window().maximize();
     wd.get(properties.getProperty("web.baseUrl"));
